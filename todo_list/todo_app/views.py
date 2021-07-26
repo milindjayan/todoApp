8f7f91e6e007a4fe79ef57bf1723d2508ec8b6d0
Milind Jayan <jayan@mbp-jayan.local>
2021-07-26 14:14:33 -0400

    Create task logic built
    Several files added and modified

--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Task
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 class TaskList(ListView):
@@ -12,3 +14,9 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name ="task"
     template_name = "todo_app/task.html"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+     
